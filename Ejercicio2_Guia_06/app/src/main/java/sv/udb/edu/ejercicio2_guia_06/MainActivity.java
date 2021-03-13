68858e04de041f14086ca94f173acf03afd4270d
Manuel Hernandez <HL180507@alumno.udb.edu.sv>
2021-03-13 15:00:32 -0600

    Agregando resolucion de guia 6, ConstrainLayouts

---
+++ Ejercicio2_Guia_06/app/src/main/java/sv/udb/edu/ejercicio2_guia_06/MainActivity.java
@@ -0,0 +1,14 @@
+package sv.udb.edu.ejercicio2_guia_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
